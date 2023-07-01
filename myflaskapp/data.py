368e171092b01b0177be8e4c547a0bcf4a76f3b2
brian-ambani <brainambani1@gmail.com>
2023-07-01 20:18:59 +0300

    updated first flask app

---
+++ myflaskapp/data.py
@@ -0,0 +1,24 @@
+def Articles():
+    articles = [
+            {
+                'id': 1,
+                'title': 'Article One',
+                'body': 'This is article one',
+                'author': 'Brain Ambani',
+                'create_date':'01-07-2023'
+                },
+            {
+                'id': 1,
+                'title': 'Article two',
+                'body': 'This is article two',
+                'author': 'Joe Doe',
+                'create_date': '01-01-2023'
+                },
+            {
+                'id': 1,
+                'title': 'Article three',
+                'body': 'This is article three',
+                'created_date': '12-12-2022'}
+                
+            ]
+    return articles
