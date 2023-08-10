56fc604b1e060370a93f040cf2fd395420b09bd3
brian-ambani <brainambani1@gmail.com>
2023-08-10 22:28:38 +0300

    updated the app file

--- tutorial/app.py
+++ tutorial/app.py
@@ -2,9 +2,15 @@ from flask import Flask
 
 app = Flask(__name__)
 
+# @ signifies a decorator - way to wrap a function and modifyinfg its behaviour
+
 @app.route('/')
 def home():
     return 'This is the home page'
+
+@app.route('/profile/<username>')
+def profile(username):
+    return "Hey there %s" % username
 
 
 if __name__ == "__main__":
