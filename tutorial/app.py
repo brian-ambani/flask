5642c2e0c7d8c14ed7523191c113f379933cb8c9
brian-ambani <brainambani1@gmail.com>
2023-08-10 23:14:16 +0300

    updated the app file

--- tutorial/app.py
+++ tutorial/app.py
@@ -1,34 +1,41 @@
-from flask import Flask, request
+from flask import Flask, render_template, request
 
 app = Flask(__name__)
 
 # @ signifies a decorator - way to wrap a function and modifyinfg its behaviour
 
-@app.route('/')
-def home():
+@app.route('/1')
+def home1():
     return 'This is the home page'
 
-@app.route('/profile/<username>')
-def profile(username):
+@app.route('/profile1/<username>')
+def profile1(username):
     return "Hey there %s" % username
 
 
-@app.route('/post/<int:post_id>')
-def show_post(post_id):
+@app.route('/post1/<int:post_id>')
+def show_post1(post_id):
     return "<h2>Post ID is %s</h2>" % post_id
 
 # Understanding request methods
 
-@app.route('/index')
-def index():
+@app.route('/index1')
+def index1():
     return "Method used: %s" % request.method
 
-@app.route("/bacon", methods=['GET', 'POST'])
-def bacon():
+@app.route("/bacon1", methods=['GET', 'POST'])
+def bacon1():
     if request.method == 'POST':
         return "You are using POST"
     else:
         return "You are using GET"
+
+
+# working with templates
+@app.route(/profile/<name>)
+def profile(name):
+    return render_template("profile.html", name=name)
+
 
 if __name__ == "__main__":
     app.run()
