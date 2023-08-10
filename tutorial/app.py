5a3a3dc333abca541ba18853def5893603e69772
brian-ambani <brainambani1@gmail.com>
2023-08-10 23:36:23 +0300

    updated the app file

--- tutorial/app.py
+++ tutorial/app.py
@@ -37,5 +37,17 @@ def profile(name):
     return render_template("profile.html", name=name)
 
 
+
+# mappint two urls in one function
+
+@app.route('/')
+@app.route('/<user>')
+def index(user=None):
+    return render_template("user.html", user=user)
+
+
+
+
+
 if __name__ == "__main__":
     app.run()
