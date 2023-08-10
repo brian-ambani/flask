4bdd831a4b1f50f3cf0d99a3ab114c1df8885c03
brian-ambani <brainambani1@gmail.com>
2023-08-10 23:02:05 +0300

    updated requests

--- tutorial/app.py
+++ tutorial/app.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask, request
 
 app = Flask(__name__)
 
@@ -16,6 +16,19 @@ def profile(username):
 @app.route('/post/<int:post_id>')
 def show_post(post_id):
     return "<h2>Post ID is %s</h2>" % post_id
+
+# Understanding request methods
+
+@app.route('/index')
+def index():
+    return "Method used: %s" % request.method
+
+@app.route("/bacon", methods=['GET', 'POST'])
+def bacon():
+    if request.method == 'POST':
+        return "You are using POST"
+    else:
+        return "You are using GET"
 
 if __name__ == "__main__":
     app.run()
