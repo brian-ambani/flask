69446a2e6ed6b51bb854028ddc04a04c90999d91
brian-ambani <brainambani1@gmail.com>
2023-08-10 22:32:26 +0300

    updated the app file

--- tutorial/app.py
+++ tutorial/app.py
@@ -13,5 +13,9 @@ def profile(username):
     return "Hey there %s" % username
 
 
+@app.route('/profile/<int:post_id>')
+def show_post(post_id):
+    return "<h2>Post ID is %s</h2>" % post_id
+
 if __name__ == "__main__":
     app.run()
